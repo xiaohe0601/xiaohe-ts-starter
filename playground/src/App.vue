4e5dd214a4b0cc090a5978690648222b28f3a88b
xiaohe0601 <xiaohe0601@outlook.com>
2025-05-06 14:12:20 +0800

    chore: update playground

--- playground/src/App.vue
+++ playground/src/App.vue
@@ -1,16 +1,7 @@
 <template>
-  <div></div>
+  <h1>playground</h1>
 </template>
 
 <script lang="ts" setup>
 
 </script>
-
-<style>
-html, body {
-  width: 100%;
-  height: 100%;
-  padding: 0;
-  margin: 0;
-}
-</style>
