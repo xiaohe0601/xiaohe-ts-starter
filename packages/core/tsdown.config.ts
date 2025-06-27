7f2ada7ccf78c3beb0fed3495f7d6b30dc66b399
xiaohe0601 <xiaohe0601@outlook.com>
2025-06-27 14:46:17 +0800

    feat: add cjs format

--- packages/core/tsdown.config.ts
+++ packages/core/tsdown.config.ts
@@ -2,5 +2,9 @@ import { defineConfig } from "tsdown";
 
 export default defineConfig({
   entry: "./src/index.ts",
-  platform: "neutral"
+  platform: "neutral",
+  format: [
+    "esm",
+    "cjs"
+  ]
 });
