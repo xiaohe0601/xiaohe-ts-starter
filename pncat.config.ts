dc4ae5ab05641694c0f1ab2951608b4d5a6f9d77
xiaohe0601 <xiaohe0601@outlook.com>
2025-11-03 11:18:39 +0800

    chore: update deps

--- pncat.config.ts
+++ pncat.config.ts
@@ -3,18 +3,11 @@ import { defineConfig, mergeCatalogRules } from "pncat";
 export default defineConfig({
   catalogRules: mergeCatalogRules([
     {
-      name: "cli",
-      match: [
-        "@antfu/ni"
-      ],
-      priority: 0
-    },
-    {
       name: "dev",
       match: [
         "@vue/runtime-core"
       ],
-      priority: 0
+      priority: 1
     },
     {
       name: "docs",
@@ -24,19 +17,26 @@ export default defineConfig({
       priority: 1
     },
     {
-      name: "types",
-      match: [
-        /\btypes\b/,
-        /\btypings\b/
-      ],
-      priority: 1
-    },
-    {
       name: "xiaohe",
       match: [
         /^@xiaohe01/
       ],
       priority: 1
+    },
+    {
+      name: "types",
+      match: [
+        /\btypes\b/,
+        /\btypings\b/
+      ],
+      priority: 10
+    },
+    {
+      name: "cli",
+      match: [
+        "@antfu/ni"
+      ],
+      priority: 20
     }
   ])
 });
